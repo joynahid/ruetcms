4f3a58154ab96fbea62bc55d3831fbcd2d71ea9d
joynahid <nahidhasan282@gmail.com>
2020-09-28 19:18:04 +0600

    trying

--- app/app_machine/vjudge_driver.py
+++ app/app_machine/vjudge_driver.py
@@ -14,16 +14,15 @@ chrome_bin_path = os.environ['CHROME_BIN']
 print(os.environ['CHROME_DRIVER_PATH'])
 print(os.environ['CHROME_BIN'])
 
-
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--disable-gpu')
-chrome_options.binary_location = chrome_bin_path
+chrome_options.binary_location = str(chrome_bin_path)
 
 webdriver = webdriver.Chrome(
-  executable_path=chrome_driver_path, options=chrome_options
+  executable_path=str(chrome_driver_path), options=chrome_options
 )
 
 def insert(contest_id, db):

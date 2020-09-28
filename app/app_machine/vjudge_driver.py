f65e923488d97a262912a5753299a3e6dc6f09f9
joynahid <nahidhasan282@gmail.com>
2020-09-28 18:44:34 +0600

    bug fixed

--- app/app_machine/vjudge_driver.py
+++ app/app_machine/vjudge_driver.py
@@ -16,7 +16,7 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--disable-gpu')
-chrome_options.binary_location(chrome_bin_path)
+chrome_options.binary_location = chrome_bin_path
 
 webdriver = webdriver.Chrome(
   executable_path=chrome_driver_path, options=chrome_options

5a8b7c0f642693f7af265f5c81fce53f392e208c
joynahid <nahidhasan282@gmail.com>
2020-09-30 23:22:17 +0600

    blog_added

--- app/blog/blog.py
+++ app/blog/blog.py
@@ -1,14 +1,16 @@
 from flask import *
 from firebase_admin import firestore
 from app.app_auth.auth import Authenticate
+import os
 
 blog = Blueprint('blog', __name__, template_folder='templates', static_folder='/static')
 
 db = firestore.client()
 
-@blog.route('/entry/<entry_uid>')
-def entry(entry_uid):
-    return render_template('eachentry.html', entry_uid = entry_uid)
+@blog.route('/post/<entry_uid>')
+def post(entry_uid):
+    post = db.collection('articles').document(entry_uid).get().to_dict()
+    return render_template('eachentry.html', post = post, entry_uid = entry_uid)
 
 @blog.route('/')
 def blogview():
@@ -22,17 +24,43 @@ def blogpost():
         text = request.form['text']
         title = request.form['title']
         tags = request.form['tags']
-        author = 'admin'
+        author = request.form['author']
+
+        oid = os.environ['BLOGPOST_ID']
+        uid = ''
+        for i in oid:
+            if i.isdigit():
+                uid+=i
+
+        uid = int(uid) + 1
+        uid = str(uid)
+
+        os.environ['BLOGPOST_ID'] = uid
 
         Tags = tags.split(',')
 
-        db.collection('articles').document('765').set({
+        db.collection('articles').document(uid).set({
             'title': title,
             'text': text,
             'author': author,
             'tags': Tags
         })
 
-        print(title, text, Tags)
+        return True
 
-    return render_template('postentry.html')
+    if Authenticate(): return render_template('postentry.html', user = session['userHandle'])
+
+    flash('Please login first', 'danger')
+    return redirect(url_for('app_auth.login'))
+
+@blog.route('/retrieveposts')
+def retposts():
+    docs = db.collection('articles').stream()
+
+    data = {}
+
+    for doc in docs:
+        rdoc = doc.to_dict()
+        data.update({doc.id:rdoc})
+    
+    return jsonify(data)

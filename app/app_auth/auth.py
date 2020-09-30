24e851193dde5fdc28d0b1231a322cae179a0728
joynahid <nahidhasan282@gmail.com>
2020-10-01 04:23:43 +0600

    no

--- app/app_auth/auth.py
+++ app/app_auth/auth.py
@@ -9,7 +9,7 @@ cred = credentials.Certificate({
     "type": "service_account",
     "project_id": os.environ['FIREBASE_PROJECT_ID'],
     "private_key_id": os.environ['FIREBASE_PRIVATE_KEY_ID'],
-    "private_key": os.environ['FIREBASE_PROJECT_KEY'],
+    "private_key": os.environ['FIREBASE_PROJECT_KEY'].replace('\\n', '\n'),
     "client_email": os.environ['FIREBASE_CLIENT_EMAIL'],
     "client_id": os.environ['FIREBASE_CLIENT_ID'],
     "auth_uri": os.environ['FIREBASE_AUTH_URI'],
